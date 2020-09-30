580b47263429c24e3af631502b317ec7d9d44b04
Nicolas <nhubert@henix.fr>
2020-09-30 16:37:59 +0200

    Coucou tu veux voir ma B*** ?

---
+++ src/test/java/org/eql/AjoutPanier.java
@@ -0,0 +1,131 @@
+package org.eql;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.*;
+
+public class AjoutPanier {
+
+	String URL = "http://192.168.1.40:8080/shopizer/shop";
+	WebDriver driver;
+	
+	
+	@Before
+	public void initialisation() {		
+			
+		//Choisir le driver parmis les 3 choix disponibles (Internet Explorer KO)
+//		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+//		System.setProperty("webdriver.ie.driver", "src/main/resources/IEDriverServer.exe");
+		
+		
+		// Instanciation du WebDriver. Choisir le driver parmis les 3 choix disponibles (Internet Explorer KO)
+//		driver = new ChromeDriver();
+		driver = new FirefoxDriver();
+//		driver = new InternetExplorerDriver();	
+	}
+		
+	
+	@Test
+	public void utilisationDuPanier() throws InterruptedException {
+
+		// instanciation de de WebDriver
+		Actions action = new Actions(driver);
+
+		// CONFIGURATION D'UN IMPLICIT WAIT
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		// PdT 1
+		// Accès à Shopizer
+		driver.get(URL);
+
+		// PdT2
+		// Cliquer sur un objet pour l'ajouter au panier
+		driver.findElement(By.xpath("//a[@class='addToCart']")).click();
+		// Vérifier que le panier a été incrémenté
+		assertEquals(driver.findElement(By.xpath("//div[@id='miniCartSummary']//strong")).getText(), "(1)");
+
+		// Récupérer le nom du produit ajouter
+		// a[@class='addToCart']/preceding::h3
+		String produitAjoute = driver.findElement(By.xpath("//a[@class='addToCart']/preceding::h3")).getText()
+				.toLowerCase();
+
+		Thread.sleep(1000);
+
+		// PdT3
+		// Selectionner le panier
+		action.moveToElement(driver.findElement(By.xpath("//div[@id='miniCartSummary']"))).build().perform();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//li[@class='checkout-bg']/a")).click();
+		// Vérifier qu'on est sur la page récapitulatif du panier
+		assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Revoir votre commande");
+
+		// PdT4
+		// Vérifier présence du produit précédemment ajouté
+		assertEquals(driver.findElement(By.xpath("//td[@data-th='Article']//strong")).getText().toLowerCase(),
+				produitAjoute);
+		// Vérification de la présence d'un tableau comprenant une image, un nom, une
+		// quantité, un prix et un sous-total par article
+		// Image
+		assertTrue(driver.findElement(By.xpath("//tr//img")).isDisplayed());
+		// Nom
+		assertTrue(driver.findElement(By.xpath("//tr//span[@class='nomargin']")).isDisplayed());
+		// Quantité
+		assertTrue(driver.findElement(By.xpath("//tr/td[@data-th='Quantité']")).isDisplayed());
+		// Prix
+		assertTrue(driver.findElement(By.xpath("//tr/td[@data-th='Prix']")).isDisplayed());
+		// Sous-Total
+		assertTrue(driver.findElement(By.xpath("//tr/td[@data-th='Total']")).isDisplayed());
+		// Récupérer le sous-total
+		String sousTotal1String = driver.findElement(By.xpath("//tr/td[@data-th='Total']/strong")).getText();
+		String sousTotal1StringOnlyNumber = sousTotal1String.substring(3);
+		double sousTotal1 = Double.parseDouble(sousTotal1StringOnlyNumber);
+
+		// PdT5
+		// Doubler la quantité du produit
+		driver.findElement(By.xpath("//tr/td[@data-th='Quantité']/input")).clear();
+		driver.findElement(By.xpath("//tr/td[@data-th='Quantité']/input")).sendKeys("2");
+		// Vérifier que seul la quantité change, pas le total.
+		String sousTotal2String = driver.findElement(By.xpath("//tr/td[@data-th='Total']/strong")).getText();
+		String sousTotal2StringOnlyNumber = sousTotal2String.substring(3);
+		double sousTotal2 = Double.parseDouble(sousTotal2StringOnlyNumber);
+		assertEquals(sousTotal2, sousTotal1, 0);
+
+		// PdT6
+		// Cliquer sur Recalculer
+		driver.findElement(By.xpath("//a[text()='Recalculer']")).click();
+		Thread.sleep(1000);
+		// Vérifier que sous total est est égale aux prix*2
+		String sousTotal3String = driver.findElement(By.xpath("//tr/td[@data-th='Total']/strong")).getText();
+		String sousTotal3StringOnlyNumber = sousTotal3String.substring(3);
+		double sousTotal3 = Double.parseDouble(sousTotal3StringOnlyNumber);
+		assertEquals(sousTotal3, sousTotal2 * 2, 0);
+
+		// PdT7
+		// Cliquer sur Proceed to checkout
+		driver.findElement(By.xpath("//div[@class='wc-proceed-to-checkout']/a")).click();
+		// Vérifier qu'on est sur la page paiement.
+		assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Paiement");
+		
+	}
+	
+	@After
+	public void Final() {
+		
+		//Quitter le navigateur
+		driver.quit();
+		
+	}
+
+}
---
+++ src/test/java/org/eql/VerifCategorieTables.java
@@ -0,0 +1,154 @@
+
+package org.eql;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.FindBy;
+
+
+public class VerifCategorieTables {
+	
+	WebDriver driver;
+	List <WebElement> produits;
+	List <WebElement> tables;
+	WebElement image;
+    WebElement prix;
+    WebElement nom;
+    WebElement ajoutPanier;
+    String prixText;
+    String nomText;
+	String URL = "http://localhost:8080/shopizer/shop/";
+	
+	
+	@Before
+	public void Initialisation() {
+		
+		//Choisir le driver parmis les 3 choix disponibles (Internet Explorer KO)
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+//		System.setProperty("webdriver.ie.driver", "src/main/resources/IEDriverServer.exe");
+		
+		// Instanciation du WebDriver. Choisir le driver parmis les 3 choix disponibles (Internet Explorer KO)
+		driver = new ChromeDriver();
+//		driver = new FirefoxDriver();
+//		driver = new InternetExplorerDriver();				
+	}
+	
+	
+	@Test		
+	public void CategorieTables () throws InterruptedException{
+	
+		//navigation
+		driver.get(URL);
+		
+		//PdT1
+		//Cliquer sur Tables
+		driver.findElement(By.xpath("//a[text()='Tables']")).click();
+		assertTrue(driver.findElement(By.xpath("//h2[text()='Tables']")).isDisplayed());
+		
+		//PdT2
+		//Vérifier la présence d'une liste de produits
+		int i = 1;
+		produits = driver.findElements(By.xpath("//div[@id='productsContainer']/div"));
+		List <Integer> idProduits = new ArrayList<Integer>();
+		for (WebElement produit:produits ) { 
+        	System.out.println(i);
+        	System.out.println(produit.getAttribute("item-name"));          
+        	if (i==1) {
+            	assertEquals(produit.getAttribute("item-name"),"Natural root console"); 
+            	String idProduitString = produit.getAttribute("data-id");
+            	int idProduitInt = Integer.parseInt(idProduitString);
+            	idProduits.add(idProduitInt);
+            }
+        	if (i==2) {
+        		assertEquals(produit.getAttribute("item-name"),"Asian rosewood console"); 
+            	String idProduitString = produit.getAttribute("data-id");
+            	int idProduitInt = Integer.parseInt(idProduitString);
+            	idProduits.add(idProduitInt);
+          	}
+        	if (i==3) {
+            	assertEquals(produit.getAttribute("item-name"),"Edge console");        	        
+            	String idProduitString = produit.getAttribute("data-id");
+            	int idProduitInt = Integer.parseInt(idProduitString);
+            	idProduits.add(idProduitInt);
+          	}
+        	if (i==4) {
+        		assertEquals(produit.getAttribute("item-name"),"Coffee table Accacia");
+        		String idProduitString = produit.getAttribute("data-id");
+        		int idProduitInt = Integer.parseInt(idProduitString);
+        		idProduits.add(idProduitInt);
+        	}
+        	image = driver.findElement(By.xpath("//*[@id='productsContainer']/div["+i+"]/div[1]"));
+        	prix = driver.findElement(By.xpath("//*[@id=\"productsContainer\"]/div["+i+"]//span"));
+        	nom = driver.findElement(By.xpath("//*[@id=\"productsContainer\"]/div["+i+"]//h3"));
+        	ajoutPanier = driver.findElement(By.xpath("//*[@id=\"productsContainer\"]/div["+i+"]//a[@class='addToCart']"));
+        	prixText = prix.getText();
+        	nomText = nom.getText();
+        	assertTrue(image.isDisplayed());
+        	assertTrue(prix.isDisplayed());
+        	assertTrue(nom.isDisplayed());
+        	assertTrue(ajoutPanier.isDisplayed());
+        	i++;
+			}
+		
+		//PdT3
+		//Selectionner le filtre Asian Wood
+		driver.findElement(By.xpath("//*[@id=\"mainContent\"]//li[3]/a")).click();
+		
+		//Vérifier le nombre d'élément est inférieur au nombre avant l'application du filtre
+		tables=driver.findElements(By.xpath("//*[@id=\"productsContainer\"]/div"));
+		assert(produits.size()>tables.size());
+	
+		//Vérifier que les tables affichées étaient bien dans la liste avant application du filtre
+		for (WebElement table:tables ) { 
+			boolean test = false;
+			String idTableString = table.getAttribute("data-id");
+			int idTable = Integer.parseInt(idTableString);
+			for (int idProduit:idProduits) {
+				if (idTable==idProduit) {
+					System.out.println("La table "+table.getAttribute("item-name")+" est bien présente après application du filtre");
+					test = true;
+				}
+			}
+			assertTrue(test);
+		}
+	
+		//PdT4
+		//Cliquer sur le premier produit
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[@id='productsContainer']//h3")).click();
+		
+		//PdT5
+		//Présence des éléments
+		String nomItem = driver.findElement(By.xpath("//h3")).getText();
+		assertEquals(nomItem.toLowerCase(),nomText.toLowerCase());
+		String prixItem = driver.findElement(By.xpath("//*[@id=\"productPrice\"]/span")).getText();
+		assertEquals(prixItem,prixText);
+		WebElement etoileItem= driver.findElement(By.xpath("//*[@class='stars']"));
+		assertTrue(etoileItem.isDisplayed());
+		WebElement ajoutPanierItem = driver.findElement(By.xpath("//button[@class='btn addToCart addToCartButton btn-buy']"));
+		assertTrue(ajoutPanierItem.isDisplayed());	
+	}
+	
+	
+	@After
+	
+	public void Final() {
+		
+		//Quitter le navigateur
+		driver.quit();
+	}
+	
+}	
